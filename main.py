67b4240b3057aefb1209e6e8df88e88bc9a6aa90
Ilnar <4sitescarp@gmail.com>
2022-06-03 14:09:27 +0300

    fff

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from flask_restful import Api, Resource, reqparse
 import random
 import quotes
 
+todos = {
+    1: {"task": "Start doing Haskell", "summary": "Do the assignment"}
+}
+
+task_post_args = reqparse.RequestParser()
+task_post_args.add_argument("task", type=str, help="Task is required", required=True)
+task_post_args.add_argument("summary", type=str, help="Summary is required", required=True)
+
 
 class Quote(Resource):
     def get(self, id=0):
@@ -14,6 +22,13 @@ class Quote(Resource):
         return "Quote not found", 404
 
     def post(self, id):
+        quote = {
+            "id": int(id),
+            "author": "fewfwe",
+            "quote": "fewfwefwef"
+        }
+        return "fewfwe", 201
+
         parser = reqparse.RequestParser()
         parser.add_argument("author")
         parser.add_argument("quote")
@@ -54,8 +69,24 @@ class Quote(Resource):
         return f"Quote with id {id} is deleted.", 200
 
 
+class ToDo(Resource):
+    def get(self, todo_id):
+        return todos[todo_id]
+
+    def post(self, todo_id):
+        args = task_post_args.parse_args()
+
+        if todo_id in todos:
+            return "This id is already taken"
+
+        todos[todo_id] = args
+
+        return todos[todo_id]
+
+
 app = Flask(__name__)
 api = Api(app)
 api.add_resource(Quote, "/ai-quotes", "/ai-quotes/", "/ai-quotes/<int:id>")
+api.add_resource(ToDo, "/to-do/<int:todo_id>")
 if __name__ == '__main__':
     app.run(debug=True)
